8b1da662df8dbd4fc0fcabefc21953f18c4b1440
Nick Gallion <nicholas.gallion@gmail.com>
2018-07-19 08:16:20 -0500

    Initial Commit

---
+++ src/main/java/org/launchcode/scheduleaider/ScheduleAiderApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.scheduleaider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleAiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleAiderApplication.class, args);
+	}
+}
